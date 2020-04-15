124341938c606814698e19103c3a532e617fd9e6
subwaydude <jwilquet@gmail.com>
2020-04-15 18:19:23 -0500

    Cleaned up old files

--- MCU/MCU.ino
+++ MCU/MCU.ino
@@ -1,4 +1,4 @@
-#define DEBUGROS 1
+//#define DEBUGROS 1
 //#define DEBUG 1
 /**
    This progarm is written for use with an Arduino MEGA 2560 r3.
@@ -31,34 +31,52 @@ int t = 0;
 double wheelDiameterMeters = 0.090;
 double wheelCircumferenceMeters = wheelDiameterMeters * 3.14;
 double ticksPerRev = 280; //From motor encoder datasheet
-double PERIOD = 100; //Period in ms for PID to sample from
+
+/**
+ * Period of system cycle. Setting this too low will result in the Arduino MEGA
+ * being overloaded and possible loss of control of robot due to a lost serial 
+ * connection between it and the rpi. This is due to the interrupt-based encoder reading
+ * taking up the time in between program execution.
+ */
+double PERIOD = 100;
+
+//Defined constants for PID
 double Kp = 2;  //Constant of Proportionality
-double Ki = 20;  //Constant of Integration
+double Ki = 10;  //Constant of Integration
 double Kd = 0;  //Constant of Derivation (ignored for this program)
-float target_linear_velocity;
-float target_angular_velocity;
+float target_linear_velocity; //read from cmd_vel
+float target_angular_velocity;  //read from cmd_vel
 
 //Speed, Distance, and Direction Variables
-double target_speed = 0; //Speed in Meters/Second (input to Arduino)
-double target_tickrate; //get tickrate for required speed
-double target_distance = 0; //Distance to move per command (input to Arduino)
-double target_ticks_to_distance = 0; //Number of encoder ticks for a given distance
+double  target_tickrate, //get tickrate for required speed
+        target_tickrate_FL,
+        target_tickrate_FR,
+        target_tickrate_RL,
+        target_tickrate_RR;
 int target_dir = 0; //Control direction
+int prev_target_dir = 0; //Store previous direction, used to tell when we need to change enable pin pattern
 
 //Individual PID controllers, one for each wheel
 double pulsePerPeriodFL = 0;
 double newPWMFL = 0;
-PID pidFL(&pulsePerPeriodFL, &newPWMFL, &target_tickrate, Kp, Ki, Kd, DIRECT);
+PID pidFL(&pulsePerPeriodFL, &newPWMFL, &target_tickrate_FL, Kp, Ki, Kd, DIRECT);
 double pulsePerPeriodFR = 0;
 double newPWMFR = 0;
-PID pidFR(&pulsePerPeriodFR, &newPWMFR, &target_tickrate, Kp, Ki, Kd, DIRECT);
+PID pidFR(&pulsePerPeriodFR, &newPWMFR, &target_tickrate_FR, Kp, Ki, Kd, DIRECT);
 double pulsePerPeriodRL = 0;
 double newPWMRL = 0;
-PID pidRL(&pulsePerPeriodRL, &newPWMRL, &target_tickrate, Kp, Ki, Kd, DIRECT);
+PID pidRL(&pulsePerPeriodRL, &newPWMRL, &target_tickrate_RL, Kp, Ki, Kd, DIRECT);
 double pulsePerPeriodRR = 0;
 double newPWMRR = 0;
-PID pidRR(&pulsePerPeriodRR, &newPWMRR, &target_tickrate, Kp, Ki, Kd, DIRECT);
-//Encoder pins, must not change A's! Maximum amount of interrupt pins are being used on the MEGA 2560
+PID pidRR(&pulsePerPeriodRR, &newPWMRR, &target_tickrate_RR, Kp, Ki, Kd, DIRECT);
+
+/**
+ * Encoder pins, must not change A's! Maximum amount of 
+ * interrupt pins are being used on the MEGA 2560. This sketch
+ * will not work if these are changed, because digital pins
+ * can not keep up with the required sampling rate, and PID control
+ * will become innacurate.
+ */
 const int FL_A = 2;
 const int FL_B = 36;
 const int FR_A = 3;
@@ -67,6 +85,7 @@ const int RL_A = 18;
 const int RL_B = 38;
 const int RR_A = 19;
 const int RR_B = 39;
+
 //pulse counters for each encoder
 volatile int pulseFL = 0;
 volatile int prevPulseFL = 0;
@@ -102,9 +121,7 @@ void int_RR()
    END INTERRUPTS
 */
 
-
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
-
 
 //declare pins for motor control
 const int motorPwmPins[] = {4, 5, 6, 7};
@@ -116,7 +133,7 @@ const int motorDirectionPins[] = {22, 23, 24, 25, 26, 27, 28, 29};
  *
  *  4 F26,  5 B27, PWM6       6 F28, 7 B29, PWM7
 */
-//Subsets of pins for movement
+//Subsets of enable pins for controlling motor directions
 const int moveRight[] = {0, 2, 5, 7};
 const int moveLeft[] = {1, 3, 4, 6};
 const int moveForward[] = {0, 3, 4, 7};
@@ -124,14 +141,14 @@ const int moveBackward[] = {1, 2, 5, 6};
 const int turnRight[] = {0, 2, 4, 6};
 const int turnLeft[] = {1, 3, 5, 7};
 
-//Feedback functions
+//Feedback variables
 std_msgs::Float32 mcBNOFeedback;
 std_msgs::Float32 mcVoltageFeedback;
 
-
+//Node handle for arduino to spin from
 ros::NodeHandle nh;
-ros::Publisher pubBNO("mcBNO", &mcBNOFeedback);
-ros::Publisher pubVoltage("mcVoltage", &mcVoltageFeedback);
+ros::Publisher pubBNO("mcBNO", &mcBNOFeedback); //Publish orientation data
+ros::Publisher pubVoltage("mcVoltage", &mcVoltageFeedback); //Publish voltage Feedback
 
 #ifdef DEBUGROS
 std_msgs::String mcDebugFeedback;
@@ -144,146 +161,14 @@ float intermediate;
 uint8_t voltageTimer = 0;
 uint8_t voltageTimerCount = 10;
 
-/**
-
-void doDirectionControl( const std_msgs::Int32 &mcSubDirection) {
-  target_dir = mcSubDirection.data;
-  
-  //Clear all enable pins before issuing a new command
-  for (int i = 0; i < 8; i++)
-  {
-    digitalWrite(motorDirectionPins[i], LOW);
-  }
-  
-  //Decoding motor direction, setting motor enable pins
-  switch (target_dir)
-  {
-    case 1: //Forward
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(motorDirectionPins[moveForward[i]], HIGH);
-      }
-      break;
-    case 2: //Reverse
-      digitalWrite(26, LOW); // solves unknown error with right weel not runnign backwards
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(motorDirectionPins[moveBackward[i]], HIGH);
-      }
-      break;
-    case 3: //Turn Right
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(motorDirectionPins[turnRight[i]], HIGH);
-      }
-      break;
-    case 4: //Turn Left
-     for (int i = 0; i < 4; i++) {
-        digitalWrite(motorDirectionPins[turnLeft[i]], HIGH);
-      }
-      break;
-    case 5: //Strafe Right
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(motorDirectionPins[moveRight[i]], HIGH);
-      }
-      break;
-    case 6: //Strafe Left
-      for (int i = 0; i < 4; i++) {
-        digitalWrite(motorDirectionPins[moveLeft[i]], HIGH);
-      }
-      break;
-    default:  //Stop
-      for (int i = 0; i < 8; i++)
-      {
-        digitalWrite(motorDirectionPins[i], LOW);
-      }
-      break;
-  }
-}
-
-void doSpeedControl(const std_msgs::Float32 mcSubSpeed) {
-  target_speed = 1.0 * mcSubSpeed.data;
-  target_tickrate = 1.0 * (((target_speed / wheelCircumferenceMeters) * ticksPerRev) / (1000 / PERIOD));
-  if (target_speed == 0)
-  {
-    newPWMFL = 0;
-    newPWMFR = 0;
-    newPWMRL = 0;
-    newPWMRR = 0;
-  }
-}
-
-void doDistanceControl(const std_msgs::Float32 mcSubDistance) {
-  target_distance = mcSubDistance.data;
-  target_ticks_to_distance = (target_distance / ( wheelCircumferenceMeters )) * ticksPerRev;
-}
-
-*/
-
+//Callback executes whenever cmd_vel is recieved
 void do_cmd_vel(const geometry_msgs::Twist cmd_vel) {
     //break down the twist angular and linear parts here
     target_linear_velocity = cmd_vel.linear.x; 
     target_angular_velocity = cmd_vel.angular.z;
-    target_tickrate = abs(1.0 * ((((target_linear_velocity + target_angular_velocity) / wheelCircumferenceMeters) * ticksPerRev) / (1000 / PERIOD)));
-
-    if (target_linear_velocity == 0 && target_angular_velocity == 0)
-    {
-      target_tickrate = 0;
-    }
-      
-
-  /*
-    //Clear all enable pins before issuing a new command
-    for (int i = 0; i < 8; i++)
-    {
-      digitalWrite(motorDirectionPins[i], LOW);
-    }
-    
-    //Decoding motor direction, setting motor enable pins
-    switch (target_dir)
-    {
-      case 1: //Forward
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[moveForward[i]], HIGH);
-        }
-        break;
-      case 2: //Reverse
-        digitalWrite(26, LOW); // solves unknown error with right weel not runnign backwards
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[moveBackward[i]], HIGH);
-        }
-        break;
-      case 3: //Turn Right
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[turnRight[i]], HIGH);
-        }
-        break;
-      case 4: //Turn Left
-       for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[turnLeft[i]], HIGH);
-        }
-        break;
-      case 5: //Strafe Right
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[moveRight[i]], HIGH);
-        }
-        break;
-      case 6: //Strafe Left
-        for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[moveLeft[i]], HIGH);
-        }
-        break;
-      default:  //Stop
-        for (int i = 0; i < 8; i++)
-        {
-          digitalWrite(motorDirectionPins[i], LOW);
-        }
-        break;
-    }
-    */
 }
 
-//ros::Subscriber<std_msgs::Int32> mcSubDirection("mcDirection", &doDirectionControl);
-//ros::Subscriber<std_msgs::Float32> mcSubSpeed("mcSpeed", &doSpeedControl);
-//ros::Subscriber<std_msgs::Float32> mcSubDistance("mcDistance", &doDistanceControl);
-
+//Subscriber that listens for a cmd_vel message and calls the above function (do_cmd_vel)
 ros::Subscriber<geometry_msgs::Twist> cmd_vel("cmd_vel", &do_cmd_vel);
 
 void setup() {  //setup code runs on device startup
@@ -316,11 +201,6 @@ void setup() {  //setup code runs on device startup
   nh.advertise(pubVoltage);
   nh.advertise(pubBNO);
   nh.subscribe(cmd_vel);
-  /*
-  nh.subscribe(mcSubDirection);
-  nh.subscribe(mcSubSpeed);
-  nh.subscribe(mcSubDistance);
-  */
 
   #ifdef DEBUGROS
   nh.advertise(pubDebug);
@@ -349,6 +229,7 @@ void setup() {  //setup code runs on device startup
     pinMode(motorDirectionPins[i], OUTPUT);
   }
 
+  //Initialize and set tunings for PID control
   pidFL.SetMode(AUTOMATIC);
   pidFL.SetTunings(Kp, Ki, Kd);
   pidFR.SetMode(AUTOMATIC);
@@ -365,7 +246,7 @@ void setup() {  //setup code runs on device startup
 
 void loop() { //loop code runs repeatedly as long as system is up
   
-  //Run the PID controller once per period
+  //System runs once per PERIOD defined at top of program. (100ms default)
   if (millis() - t > PERIOD)
   {
     #ifdef DEBUG
@@ -390,17 +271,24 @@ void loop() { //loop code runs repeatedly as long as system is up
     bno.getEvent(&event);
     mcBNOFeedback.data = event.orientation.x;
     pubBNO.publish(&mcBNOFeedback);
+
+    /**
+     * Debug PID controllers
+     * Compares desired tickrate (commanded) to instantaneously calculated tickrate (real)
+     * Also shows linear and angular velocity components reseved from Raspberry Pi
+     * To view this info, debug on PC and echo the "mcDebugFeedback" topic
+     */
     #ifdef DEBUGROS
-    String debugData =  "Liear Velocity: " + String(target_linear_velocity,4) + 
-                        " Angular Velocity: " + String(target_angular_velocity,4);
-    
-    /*"FL_ACTUAL_TICKRATE: " + String(pulsePerPeriodFL,4)
-                       + "\tDESIRED_TICKRATE : " + String(target_tickrate, 4);
-                       */
-    mcDebugFeedback.data = debugData.c_str();
+    String debugData =  "Liear Velocity: " + String(target_linear_velocity, 4) + 
+                        " Angular Velocity: " + String(target_angular_velocity, 4) +
+                        " FL_ACTUAL_TICKRATE: " + String(pulsePerPeriodFL, 4) +
+                        " DESIRED_TICKRATE: " + String(target_tickrate, 4);
+ 
+    mcDebugFeedback.data = debugData.c_str(); //the string defined for rosserial works with c_strings, convert it
     pubDebug.publish(&mcDebugFeedback);
     #endif
-    
+
+    //Calculate instantaneous pulse rates
     pulsePerPeriodFL = (pulseFL - prevPulseFL);
     prevPulseFL = pulseFL;
     pulsePerPeriodFR = (pulseFR - prevPulseFR);
@@ -410,52 +298,172 @@ void loop() { //loop code runs repeatedly as long as system is up
     pulsePerPeriodRR = (pulseRR - prevPulseRR);
     prevPulseRR = pulseRR;
 
-    
-    if (target_angular_velocity > 0.0 && target_linear_velocity == 0.0)
+    //Calculate desired base tick rate, if robot is moving
+    if (target_angular_velocity == 0 && target_linear_velocity == 0)
     {
-      //turn left
-      target_dir = 3;
-      for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[turnLeft[i]], HIGH);
-      }
-    }
-    else if (target_angular_velocity < 0.0 && target_linear_velocity == 0.0)
-    {
-      //turn right
-      target_dir = 4;
-      for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[turnRight[i]], HIGH);
-      }
-    }
-    else if (target_linear_velocity > 0.0 && target_angular_velocity == 0.0)
-    {
-      //drive straight
-      target_dir = 1;
-      for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[moveForward[i]], HIGH);
-      }
-    }
-    else if (target_linear_velocity < 0.0 && target_angular_velocity == 0.0)
-    {
-      //drive backwards
-      target_dir = 2;
-       for (int i = 0; i < 4; i++) {
-          digitalWrite(motorDirectionPins[moveBackward[i]], HIGH);
-       }
-    }
-    else {
-      //Don't move
+      //robot is stopped
+      target_tickrate = 0;
       target_dir = 0;
-      for(int i = 0; i < 8; i++) {
-        digitalWrite(motorDirectionPins[i], LOW);
+    } else 
+    {
+      //Robot is moving, decide what kind of motion we have (angular, linear, or both)
+      if (target_linear_velocity == 0)
+      {
+        //we are purely angular (rotation but no translation)
+        target_tickrate = abs(1.0 * (((target_angular_velocity / 
+        wheelCircumferenceMeters) * ticksPerRev) / (1000 / PERIOD)));
+      }
+      else
+      {
+        /**
+         * we have linear motion (translation), and may have rotation (angular) as well.
+         * But we will use the linear velocity as our base and use the angular velocity to modify it.
+         */
+        target_tickrate = abs(1.0 * (((target_linear_velocity / 
+        wheelCircumferenceMeters) * ticksPerRev) / (1000 / PERIOD)));
+      }
+      
+    }
+    
+    //Set target_tickrate for each wheel based on direction
+    if (target_linear_velocity > 0)
+    {
+      target_dir = 1; //If the linear velocity is positive, the robot is moving forward
+      if (target_angular_velocity == 0) //if there is no angular, it is moving straight
+      {
+        //forward
+        target_tickrate_FL = target_tickrate;
+        target_tickrate_FR = target_tickrate;
+        target_tickrate_RL = target_tickrate;
+        target_tickrate_RR = target_tickrate;
+      }
+      else if (target_angular_velocity > 0) //negative angular means veering towards the left
+      {
+        //forward and left
+        target_tickrate_FL = target_tickrate * abs(target_angular_velocity);
+        target_tickrate_FR = target_tickrate;
+        target_tickrate_RL = target_tickrate * abs(target_angular_velocity);
+        target_tickrate_RR = target_tickrate;
+        
+      } 
+      else //The anular velocity must be positive, so we are veering towards the right
+      {
+        //forward and right
+        target_tickrate_FL = target_tickrate;
+        target_tickrate_FR = target_tickrate * abs(target_angular_velocity);
+        target_tickrate_RL = target_tickrate;
+        target_tickrate_RR = target_tickrate * abs(target_angular_velocity);
+      }
+    } 
+    else if (target_linear_velocity < 0)  //A negative linear velocity means we are reversing
+    {
+      target_dir = 2;
+      if (target_angular_velocity == 0) //No angular velocity means we are moving straight back
+      {
+        //backwards
+        target_tickrate_FL = target_tickrate;
+        target_tickrate_FR = target_tickrate;
+        target_tickrate_RL = target_tickrate;
+        target_tickrate_RR = target_tickrate;
+      }
+      else if (target_angular_velocity > 0) //A positive angular velocity means we are veering left
+      {
+        //backwards and left
+        target_tickrate_FL = target_tickrate * abs(target_angular_velocity);
+        target_tickrate_FR = target_tickrate;
+        target_tickrate_RL = target_tickrate * abs(target_angular_velocity);
+        target_tickrate_RR = target_tickrate;
+      } 
+      else //We must have a negative angular velocity, so we are veering right
+      {
+        //backwards and right
+        target_tickrate_FL = target_tickrate;
+        target_tickrate_FR = target_tickrate * abs(target_angular_velocity);
+        target_tickrate_RL = target_tickrate;
+        target_tickrate_RR = target_tickrate * abs(target_angular_velocity);
+      }
+    } 
+    else 
+    {
+      //Robot is not moving foward or stopped, so it could be turning
+      if (target_angular_velocity > 0) //Positive angular means turn left
+      {
+        //turning left
+        target_dir = 3;
+        target_tickrate_FL = target_tickrate;
+        target_tickrate_FR = target_tickrate;
+        target_tickrate_RL = target_tickrate;
+        target_tickrate_RR = target_tickrate;
+      }
+      else if (target_angular_velocity < 0) //Negative angular means turn right
+      {
+        //turning right
+        target_dir = 4;
+        target_tickrate_FL = target_tickrate;
+        target_tickrate_FR = target_tickrate;
+        target_tickrate_RL = target_tickrate;
+        target_tickrate_RR = target_tickrate;
       }
     }
-   
 
+    //Read the target direction and enable the correct pins for that movemenet pattern
+    if (target_dir != prev_target_dir) {
+      prev_target_dir = target_dir;
+      if (target_dir == 0)  //Reset all enable pins to stop robot
+      {
+        //stop
+        for(int i = 0; i < 8; i++) 
+        {
+          digitalWrite(motorDirectionPins[i], LOW);
+        }
+      } else if (target_dir == 1)
+      {
+        for (int i = 0; i < 4; i++) 
+        {
+          digitalWrite(motorDirectionPins[moveForward[i]], HIGH);
+        }
+      } else if (target_dir == 2)
+      {
+        for (int i = 0; i < 4; i++) 
+        {
+          digitalWrite(motorDirectionPins[moveBackward[i]], HIGH);
+        }
+      } else if (target_dir == 3)
+      {
+        for (int i = 0; i < 4; i++) 
+        {
+          digitalWrite(motorDirectionPins[turnLeft[i]], HIGH);
+        }
+      } else if (target_dir == 4)
+      {
+        for (int i = 0; i < 4; i++) 
+        {
+          digitalWrite(motorDirectionPins[turnRight[i]], HIGH);
+        }
+      } else  //Stop if we recieve an unknown value
+      {
+         //stop
+        for(int i = 0; i < 8; i++)
+        {
+          digitalWrite(motorDirectionPins[i], LOW);
+        }
+      }
+    }
+    
     //run PID controllers only when needed. Prevents jumping when changing directions due to PID overcompensation
+    //(The PID controller will think a wheel is stuck when in reality, its enable pin is off so it can't spin
+    //This causes the PID to send 255 full duty cycle to motor, which will cause a rough start
     switch(target_dir)
     {
       case 0:
+        pidFL.SetMode(AUTOMATIC);
+        pidFL.SetTunings(Kp, Ki, Kd);
+        pidFR.SetMode(AUTOMATIC);
+        pidFR.SetTunings(Kp, Ki, Kd);
+        pidRL.SetMode(AUTOMATIC);
+        pidRL.SetTunings(Kp, Ki, Kd);
+        pidRR.SetMode(AUTOMATIC);
+        pidRR.SetTunings(Kp, Ki, Kd);
         break;
       case 1:
       case 2:
@@ -489,7 +497,6 @@ void loop() { //loop code runs repeatedly as long as system is up
       Serial.println(newPWMFL);
     #endif
     
-      
     t = millis();
 
     /**
